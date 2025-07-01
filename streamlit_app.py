5909ec99b61fe5005cf43a120739911133070010
James Kilonzo <121459880+ex-rnd@users.noreply.github.com>
2025-07-01 16:13:56 +0300

    Using streamlit expander

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,11 @@ st.title(' 🚀 Machine Learning Application')
 
 st.info(" This app builds a machine learning model! ")
 
-df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/streamlit_freecodecamp/d44c4c1320f8417b3d5494902e5366b715314528/app_8_classification_penguins/penguins_cleaned.csv')
 
-df
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/streamlit_freecodecamp/d44c4c1320f8417b3d5494902e5366b715314528/app_8_classification_penguins/penguins_cleaned.csv')
+  df
+
+
+
