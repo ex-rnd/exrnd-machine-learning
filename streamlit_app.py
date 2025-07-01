038f74736b37c8d4d2f3b8a4a20bc9190540b2f5
James Kilonzo <121459880+ex-rnd@users.noreply.github.com>
2025-07-01 16:11:23 +0300

    Pandas dataframe display in streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title(' 🚀 Machine Learning Application')
 
-st.info(" This app builds a mchine learning model! ")
+st.info(" This app builds a machine learning model! ")
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/streamlit_freecodecamp/d44c4c1320f8417b3d5494902e5366b715314528/app_8_classification_penguins/penguins_cleaned.csv')
+
+df
